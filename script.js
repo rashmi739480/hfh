075a27c52b59b19194dd36f9b6ef18cc205f8e06
rashmi739480 <112721809+rashmi739480@users.noreply.github.com>
2024-11-11 14:59:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// API for get requests
+let fetchRes = fetch(
+  "https://jsonplaceholder.typicode.com/todos/1");
+      
+  // FetchRes is the promise to resolve
+  // it by using.then() method
+  fetchRes.then(res =>
+      res.json()).then(d => {
+          console.log(d)
+      })
+  
+
